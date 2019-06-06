d89260189fa4672972a53b2bd843cdc2219243c2
Daniel Rines <drines@gmail.com>
2019-06-06 16:54:58 -0700

    feat: added the move function code
    Implements robot motion by updating the beliefs matrix based on
    the intended dx and dy of the robot.

--- localizer.cpp
+++ localizer.cpp
@@ -38,8 +38,6 @@ using namespace std;
            0.25 0.25
 */
 vector< vector <float> > initialize_beliefs(vector< vector <char> > grid) {
-	vector< vector <float> > newGrid;
-
 	// initialize local variables for the grid variables
 	int height = grid.size();
     int width = grid[0].size();
@@ -49,15 +47,13 @@ vector< vector <float> > initialize_beliefs(vector< vector <char> > grid) {
     float belief_per_cell = 1.0 / area;
     
 	// newGrid with uniform ratio values populating all of the newGrid cells
-	vector < vector<float> > newGrig (height, vector<float>(width, belief_per_cell));
+	vector < vector<float> > newGrid (height, vector<float>(width, belief_per_cell));
 	
 	// returns the uniform newGrid structure
 	return newGrid;
 }
 
 /**
-  TODO - implement this function 
-    
     Implements robot motion by updating beliefs based on the 
     intended dx and dy of the robot. 
 
@@ -94,14 +90,33 @@ vector< vector <float> > initialize_beliefs(vector< vector <char> > grid) {
 */
 vector< vector <float> > move(int dy, int dx, 
   vector < vector <float> > beliefs,
-  float blurring) 
-{
+  float blurring) {
+	
+  	// initialize local variables based on the beliefs matrix dimensions
+  	int height = beliefs.size();
+  	int width = beliefs[0].size();
 
-  vector < vector <float> > newGrid;
+	// construct a newGrid that is the same size as the beliefs matrix
+	// and fully initialize it with zeros:
+	vector < vector <float> > newGrid (height, vector <float> (width, 0.0));
 
-  // your code here
+	// create local variables for new position iterators (new_i, new_j)
+	int new_i = 0;
+	int new_j = 0;
 
-  return blur(newGrid, blurring);
+	// loop thru the beliefs matrix and shift cell values based on dy, dx
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			// calculate the frame shift for newGrid
+			new_i = (i + dy) % width;
+			new_j = (j + dx) % height;
+			// store the value from beliefs into the correct newGrid pos.
+			newGrid[new_i][new_j] = beliefs[i][j];
+		}
+	}
+
+	// return the newGrid matrix + passed in blurring value
+	return blur(newGrid, blurring);
 }
 
 
